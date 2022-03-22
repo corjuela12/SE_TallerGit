86deb97b53febc165cf7210a7fc39d98ff221358
unknown <mitchellorjuela@gmail.com>
2022-03-21 21:14:30 -0500

    se agrega --name como arguemnto del programa

--- hola.py
+++ hola.py
@@ -1,5 +1,12 @@
+import argparse
+
 def say_hello(name="World"):
+    print("Hello  ", name, "!")
 
-    print("Hello  ", name, " ! ")
+parser = argparse.ArgumentParser(description='say hello.')
 
-    say_hello()
+parser.add_argument('--name', type=str, required=False,
+                    metavar='name', help='your name',default='Mitchell')
+args= parser.parse_args()
+
+say_hello(args.name)
